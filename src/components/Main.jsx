de303ccfd4f856ca698ad539c544ab23af11e04c
Harry <harryrock67@gmail.com>
2019-02-19 16:00:04 -0500

    props is functional added images to the todoList debugging src usage from props

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,10 +6,14 @@ const Main = () => {
         <div>
             <h1>Checklist</h1>
                 <ToDoItem
-                    props={{name: "item", src:"item1.jpg", alt:"ocean view"}}
+                    info={{name: "item 1", src:"../resources/img/item1.jpg", alt:"ocean view"}}
                 />
-                <ToDoItem/>
-                <ToDoItem/>
+                <ToDoItem
+                    info={{name: "item 2", src:"../resources/img/item2.jpg", alt:"ocean view"}}
+                />
+                <ToDoItem
+                    info={{name: "item 3", src:"../resources/img/item3.jpg", alt:"ocean view"}}
+                />
         </div>
     )
 }
